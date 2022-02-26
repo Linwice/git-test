6c8c90c68d9db81069f038716d03d6e4060ffdd0
linwice <949589397@qq.com>
2022-02-26 23:49:15 +0800

    first commit

---
+++ src/main/java/com/linwice/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.linwice.git;
+
+/**
+ * @Author: LinwiceLee
+ * @Date: 2022/2/26 23:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
